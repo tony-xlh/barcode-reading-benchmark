1b13bfda6c0a251e1595f6336e08d95ca966e61c
xulihang <xulihanghai@163.com>
2023-04-14 15:48:04 +0800

    add a floating action button

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,16 @@
 <template>
   <q-page class="row items-center justify-evenly">
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+      <q-btn fab icon="add" color="blue" v-on:click="AddButtonClicked" />
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-
+const AddButtonClicked = () => {
+  console.log("add");
+}
 
 </script>
