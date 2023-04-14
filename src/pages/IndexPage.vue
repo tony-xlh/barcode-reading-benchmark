596e94f7480f94470c7da0cea501c592a0899d9f
xulihang <xulihanghai@163.com>
2023-04-14 16:58:47 +0800

    add a dialog to create new projects

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -15,25 +15,34 @@
         </q-item>
       </q-list>
     </div>
+    <q-dialog v-model="newProject">
+      <q-card>
+        <q-card-section>
+          <q-input outlined v-model="projectName" label="Project Name" />
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="primary" v-close-popup />
+          <q-btn flat label="Okay" color="primary" v-close-popup v-on:click="createProject" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
-      <q-btn fab icon="add" color="blue" v-on:click="AddButtonClicked" />
+      <q-btn fab icon="add" color="blue" v-on:click="addButtonClicked" />
     </q-page-sticky>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { timeStamp } from "console";
 import { Project } from "../definitions/project";
 import localForage from "localforage";
 import { onMounted, ref } from "vue";
+const newProject = ref(false);
+const projectName = ref("");
 const projects = ref([] as Project[]);
 onMounted(async () => {
-  const savedProjects = await localForage.getItem("projects") as Project[];
+  const savedProjects = await localForage.getItem("projects");
   if (savedProjects) {
-    projects.value = savedProjects;
-  }else{
-    let testProject = new Project({name:"Test",creationTimestamp:1681459465815,images:[]});
-    projects.value = [testProject];
+    projects.value = JSON.parse(savedProjects as string);
   }
 });
 
@@ -41,8 +50,38 @@ const getLocalTime = (timestamp:number) => {
   return new Date(timestamp).toLocaleString();
 }
 
-const AddButtonClicked = () => {
-  console.log("add");
+const addButtonClicked = () => {
+  projectName.value = "";
+  newProject.value = true;
+}
+
+const createProject = () => {
+  if (hasDuplicateName(projectName.value)) {
+    alert("Project name duplicated. Please use another name.");
+  }else{
+    const project = new Project({name:projectName.value,creationTimestamp:Date.now(),images:[]});
+    projects.value.push(project);
+    saveProjects();
+  }
+}
+
+const hasDuplicateName = (name:string) => {
+  for (let index = 0; index < projects.value.length; index++) {
+    const project = projects.value[index];
+    if (project.info.name === name) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const saveProjects = async () => {
+  console.log(projects.value);
+  let projectsToSave:Project[] = [];
+  projects.value.forEach(project => {
+    projectsToSave.push(project);
+  });
+  await localForage.setItem("projects", JSON.stringify(projectsToSave));
 }
 
 </script>
