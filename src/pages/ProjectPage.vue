6cde2966364c5a68a438ddd2528a484c987f4c6b
xulihang <xulihanghai@163.com>
2023-04-14 18:01:43 +0800

    add a table

--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -1,11 +1,35 @@
 <template>
   <q-page class="row justify-evenly">
+    <div class="q-pa-md" style="width:100%;">
+      <q-table
+        title="Images"
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+      />
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Filename',
+    align: 'left',
+    field: 'filename',
+    sortable: true
+  }
+]
+
+const rows = [
+  {
+    filename: 'image.jpg',
+  }
+]
 const router = useRouter();
 const projectName = ref("");
 onMounted(async () => {
