1df0ca311827410cee47b8517dff216822efb7bb
xulihang <xulihanghai@163.com>
2023-04-24 10:54:01 +0800

    remove unneeded properties of Project

--- src/project.ts
+++ src/project.ts
@@ -1,20 +1,7 @@
 export class Project {
   info:ProjectInfo;
-  baseURL = "";
   constructor(info:ProjectInfo){
     this.info = info;
-  }
-
-  setBaseURL(baseURL:string){
-    this.baseURL = baseURL;
-  }
-
-  getImageLink(image:string){
-    return this.baseURL + "/" + image;
-  }
-
-  getAnnotationsLink(){
-    return this.baseURL + "/annotations.zip";
   }
 }
 
