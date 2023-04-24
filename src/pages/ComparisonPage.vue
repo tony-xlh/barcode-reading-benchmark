813037dbafafae47d494788216b77588016c5e40
xulihang <xulihanghai@163.com>
2023-04-24 11:20:03 +0800

    comparison page: add checkboxes of engines

--- src/pages/ComparisonPage.vue
+++ src/pages/ComparisonPage.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-
-  </div>
+  <q-page class="row justify-evenly">
+    <q-card flat bordered class="overview-card" style="width:100%;">
+      <q-card-section>
+        <div class="text-h6">{{projectName}}</div>
+      </q-card-section>
+      <q-separator></q-separator>
+      <q-card-section>
+        <div>
+          Engines:
+          <q-checkbox v-model="engine.enabled" :label="engine.name" v-for="engine in engines" v-bind:key="engine.name"/>
+        </div>
+        <q-btn outline color="primary" label="Get comparison statistics" v-on:click="getStatistics()" />
+      </q-card-section>
+    </q-card>
+  </q-page>
 </template>
 
 <script setup lang="ts">
@@ -11,13 +23,19 @@ import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import localForage from "localforage";
 const projectName = ref("");
-const engines = ref([] as string[])
+const engines = ref([] as {name:string,enabled:boolean}[])
 const router = useRouter();
 let project:Project;
 onMounted(async () => {
   projectName.value = router.currentRoute.value.params.name as string;
   const supportedEngines = BarcodeReader.getEngines();
-  engines.value = supportedEngines;
+  const enginesList = [];
+  for (let index = 0; index < supportedEngines.length; index++) {
+    const engine = supportedEngines[index];
+    const item = {name:engine,enabled:true};
+    enginesList.push(item);
+  }
+  engines.value = enginesList;
   const savedProjects = await localForage.getItem("projects");
   if (savedProjects) {
     const projects = JSON.parse(savedProjects as string);
@@ -29,4 +47,8 @@ onMounted(async () => {
     }
   }
 });
+
+const getStatistics = () => {
+  console.log(engines.value);
+}
 </script>
