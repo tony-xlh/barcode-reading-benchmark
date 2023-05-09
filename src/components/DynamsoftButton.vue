9b1a5a25b13f6eef042931e54518b7e8a5e92a90
xulihang <xulihanghai@163.com>
2023-05-09 15:45:23 +0800

    add outline prop to Dynamsoft Button

--- src/components/DynamsoftButton.vue
+++ src/components/DynamsoftButton.vue
@@ -1,11 +1,20 @@
 <script setup>
-defineProps(['label'])
+const props = defineProps(['label','outline'])
 defineEmits(['click'])
+
+const getClassName = () => {
+  console.log(props);
+  if (props.outline === '') {
+    return 'outline textUpperCase fontOswald orangeBgBtn';
+  }else{
+    return 'textUpperCase fontOswald orangeBgBtn';
+  }
+}
 </script>
 
 <template>
   <a 
-    class="textUpperCase fontOswald orangeBgBtn" 
+    :class="getClassName()" 
     style="transition:ease-in 0.2s all"
     @click="$emit('click', $event.target.value)"
   >
@@ -19,15 +28,22 @@ defineEmits(['click'])
 }
 
 .orangeBgBtn {
+  display: inline-block;
   padding: 5px 10px;
   background-color: #fe8e14;
-  border-color: #fe8e14;
   color: #fff;
   text-align: center;
+  border: 2px solid #fe8e14;
 }
 
 .orangeBgBtn:hover {
   box-shadow: -4px 4px 0 0 #000;
   transform: translate(4px,-4px);
 }
+.outline {
+  color: #fe8e14;
+  border-color: #fe8e14;
+  background-color: #fff;
+}
+
 </style>
