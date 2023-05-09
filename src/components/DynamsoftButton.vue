c24267b10d2942d011a1c7324215356021558c16
xulihang <xulihanghai@163.com>
2023-05-09 15:27:32 +0800

    add dynamsoft button component

---
+++ src/components/DynamsoftButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps(['label'])
+defineEmits(['click'])
+</script>
+
+<template>
+  <a 
+    class="textUpperCase fontOswald orangeBgBtn" 
+    style="transition:ease-in 0.2s all"
+    @click="$emit('click', $event.target.value)"
+  >
+    {{ label }}
+  </a>
+</template>
+
+<style scoped>
+.textUpperCase {
+  text-transform: uppercase;
+}
+
+.orangeBgBtn {
+  padding: 5px 10px;
+  background-color: #fe8e14;
+  border-color: #fe8e14;
+  color: #fff;
+  text-align: center;
+}
+
+.orangeBgBtn:hover {
+  box-shadow: -4px 4px 0 0 #000;
+  transform: translate(4px,-4px);
+}
+</style>
